4b81236f5552d0377d0b0d585dcd1a7fa9b02d5e
Plamen Karolev <asenov78@gmail.com>
2026-06-17 08:52:31 +0300

    ci: trigger v1.10.0 release build

--- config/dravion.php
+++ config/dravion.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '1.10.0',
+    'version' => '1.10.0', // trigger release workflow
 
     'license_server' => env('DRAVION_LICENSE_SERVER', 'https://apsbg.com/dravion-server'),
     'updates_server' => env('DRAVION_UPDATES_SERVER', 'https://apsbg.com/dravion-server'),
